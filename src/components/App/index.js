59087330987b6bb4888d20829043c6b61c2fbaf5
JeremyXZ <jeremyuk168@gmail.com>
2022-11-22 11:52:18 +0000

    html structure

---
+++ src/components/App/index.js
@@ -0,0 +1,49 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+          <div>logo</div>
+          <h1>Quiz Kaufman</h1>
+      </div> 
+      <section>
+        <form className="form">
+          <input></input>
+          <input></input>
+          <input></input>
+          <input></input>
+          <div className="button-container">
+            <button>click me</button>
+            <button>click</button>
+            <button>click</button>
+          </div>                 
+        </form>
+      </section>
+
+      <section className="quiz-body">
+        <div className="quiz-generator">
+          <form>
+            <label> topic1
+            <input type="checkbox"></input>
+            </label>
+            <label> topic2
+            <input type="checkbox"></input>
+            </label>
+            <label> topic3
+            <input type="checkbox"></input>
+            </label>
+            <label> topic4
+            <input type="checkbox"></input>
+            </label>
+          </form>
+        </div>
+      </section>
+    </div>
+    
+
+  );
+}
+
+export default App;
