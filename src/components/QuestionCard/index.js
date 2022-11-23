7f1e63a33b708db9b7be2ef971e42f8d40d3f8f8
Kylie Smaldon <k_smaldon@outlook.com>
2022-11-23 17:56:29 +0000

    added functionality to reveal answer button

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -1,14 +1,24 @@
 import Button from "../Button"
+import { useState } from "react";
+import './index.css'
 export default function QuestionCard({question, answer}) {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = event => {
+        // 👇️ toggle isActive state on click
+        setIsActive(current => !current);
+      };
 
     return(
+    
         <div className='question-card'>
           <h4>Question</h4> 
           <Button text="Delete"/>
            <p>{question}</p>
            <h4>Answer</h4>
-           <p>{answer}</p>
-           <Button text="Reveal Answer"/>
+           <p className={isActive ? 'visible' : 'hidden'}>{answer}</p>
+           {/* <Button text="Reveal Answer"/> */}
+           <button  onClick={handleClick}>Reveal Answer</button>
         </div>
     )
 }
