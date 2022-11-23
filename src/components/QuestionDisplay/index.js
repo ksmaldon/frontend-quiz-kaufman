81270bbfc6d45e3fd36f880e78f9f782e90c3605
Kylie Smaldon <k_smaldon@outlook.com>
2022-11-23 17:21:25 +0000

    added question mapping

--- src/components/QuestionDisplay/index.js
+++ src/components/QuestionDisplay/index.js
@@ -1,18 +1,18 @@
-// import QuestionCard from "../QuestionCard";
+import QuestionCard from "../QuestionCard";
 
 export default function QuestionDisplay({ questions }) {
   // mapping into QuesitonCard
   return (
     <div>
-      {questions.length > 0 && <pre>{JSON.stringify(questions, null, 2)}</pre>}
-      {/* {fiveQuestionsState.map((questionObject)=>{
+      {/* {questions.length > 0 && <pre>{JSON.stringify(questions, null, 2)}</pre>} */}
+      {questions.map((questionObject)=>{
                 return <QuestionCard 
                 key ={questionObject.question_id} 
                 question={questionObject.question} 
                 answer={questionObject.answer}
 
                 />
-            })} */}
+            })} 
     </div>
   );
 }
