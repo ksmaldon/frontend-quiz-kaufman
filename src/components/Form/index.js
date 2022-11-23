0388a4667180a43925845e63adf73f029e7d28e0
Muhammed Atyib Ali <aatyib@gmail.com>
2022-11-23 12:49:00 +0000

    handle change added

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,22 +1,44 @@
 import './index.css'
 import Button from "../Button"
+import { useState } from 'react'
+
+//PLAN
+//Created our branch 
+//Create a handle change function to get the data from the form 
+//onclickbutton with useState  that will send our post to the function 
+//have A edit and add button
+//Create two function one for POST and PATCH
+
 
 export default function Form() {
+    const [formData, setFormData]= useState({Questions:"",Answer:"",Topic:""})
+
+    function handleChangeForm(event) {
+        const Question = event.target.form.Questions.value
+        const Answers = event.target.form.Answer.value
+        const Topics = event.target.form.Topic.value
+
+        setFormData({Questions:Question,Answer:Answers,Topic:Topics})
+        console.log(formData)
+
+    }
+
+
 
     return (
         <div>
-            <form className="form">
+            <form onChange={handleChangeForm} className="form">
                 <div className="input-field-container">
                     <label>Question:
-                        <input></input>
+                        <input name="Questions"></input>
                     </label>
                     
                     <label>Answer:
-                        <input></input>
+                        <input  name="Answer" ></input>
                     </label>
 
                     <label>Topic:
-                        <input></input>
+                        <input  name="Topic"></input>
                     </label>   
                 </div>
                 
