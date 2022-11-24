db4f4f1b29623f3848d676a35bb55331ec78a4c1
ionarose <ionaroseuk@gmail.com>
2022-11-24 14:23:30 +0000

    logo done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,9 +4,10 @@ export default function Header({title}) {
 
     return (
         <div className="header">
-           <div className="logo"></div>
-         <img src={logo} />
-           <h1>{title}</h1>
+           <div className="logo">
+         <img className="logo-image" src={logo} />
+         </div>
+           
      </div> 
     )
 }
