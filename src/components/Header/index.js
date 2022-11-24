0e4aaac4e72b9959c667f2ca47ed16f8f45031cd
ionarose <ionaroseuk@gmail.com>
2022-11-24 14:10:05 +0000

    dropdown fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import './index.css'
-
+import logo from "../images/quiz_kaufman_logo.png" 
 export default function Header({title}) {
 
     return (
         <div className="header">
-           <img src=""/>
+           <div className="logo"></div>
+         <img src={logo} />
            <h1>{title}</h1>
      </div> 
     )
