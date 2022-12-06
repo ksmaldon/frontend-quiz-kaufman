f891b819e40cdc2e466d5ed64a51942c87173938
Kylie Smaldon <k_smaldon@outlook.com>
2022-12-06 15:07:15 +0000

    updated url

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,19 +3,20 @@ import Header from "../Header";
 import Form from "../Form";
 import QuizBody from "../QuizBody";
 import { useEffect, useState } from "react";
+const url = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3000";
 
 function App() {
   const [allQuestions, setAllQuestions] = useState([]);
 
   async function deleteQuestion(id) {
-    const response = await fetch(`/api/questions/${id}`, { method: "DELETE" });
+    const response = await fetch(`${url}/api/questions/${id}`, { method: "DELETE" });
     const data = await response.json();
     console.log(data);
   }
 
   useEffect(() => {
     async function getTopics() {
-      const response = await fetch("/api/questions");
+      const response = await fetch(`${url}/api/questions`);
       const data = await response.json();
       setAllQuestions(data.payload);
     }
